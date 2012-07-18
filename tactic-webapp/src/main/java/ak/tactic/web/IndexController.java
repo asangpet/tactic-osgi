04fb6efbbb0398a72c0becc74afe9af82160e250
Akkarit Sangpetch <asangpet@gmail.com>
2012-07-17 23:50:14 -0700

    Added web project

---
+++ tactic-webapp/src/main/java/ak/tactic/web/IndexController.java
@@ -0,0 +1,37 @@
+package ak.tactic.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ak.tactic.data.DataService;
+import ak.tactic.data.ResponseInfo;
+
+@Controller
+public class IndexController {
+	Logger log = LoggerFactory.getLogger(IndexController.class);
+	
+	@Autowired
+	private DataService dataService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		//log.info(helloService.sayHello("Hello world"));
+		String result = "";
+		for (ResponseInfo info : dataService.getResponses()) {
+			result += info.toString();
+		}
+		model.addAttribute("helloLabel", result);
+		//model.addAttribute("helloLabel", "Not much here, move along");
+		return "index";
+	}
+	
+	@RequestMapping(value = "/play", method = RequestMethod.GET)
+	public String play(Model model) {
+		return "play";
+	}
+}
